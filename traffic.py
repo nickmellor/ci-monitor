8f1755623e1995775e05014f076e5c405b7b9d9b
Nick Mellor <nicholas.mellor@medibank.com.au>
2015-10-06 07:39:41 +1100

    light patterns named and moved to conf

--- traffic.py
+++ traffic.py
@@ -6,18 +6,6 @@ from conf import conf
 from time import sleep
 
 
-traffic_light_lamp_configs = {
-    'green': (0, 0, 1),
-    'yellow': (0, 1, 0),
-    'red': (1, 0, 0),
-    'off': (0, 0, 0),
-    'all': (1, 1, 1),
-    'redyellow': (1, 1, 0),
-    'commserror': (1, 1, 0),
-    'changestate': (1, 0, 1),
-}
-
-
 class TrafficLight:
     """
     use one class instance per traffic light device
@@ -40,33 +28,30 @@ class TrafficLight:
 
     def change_lights(self, new_colour):
         # ignore requests to change lights if last run had an unhandled exception
-        # lights quietly stay all on rather than doing the Christmas Tree lights thing
-        # every few seconds
-        # TODO: unhandled_exceptions check belongs in ci_monitor.py and bamboo.py
+        # (wait for stable recovery)
         if self.finished_without_unhandled_exceptions:
-            self.blink() # check traffic app hasn't crashed
+            self.blink()  # visual check to make sure ci-monitor hasn't crashed
             if new_colour != self.old_colour:
-                # draw some attention to change
                 self.draw_attention_to_state_change()
-                logger.info('Light changing from {0} to {1}!'.format(self.old_colour, new_colour))
+                logger.info('Light changing from {0} to {1}'.format(self.old_colour, new_colour))
                 self.old_colour = new_colour
                 sound.play_sound(self.old_colour, new_colour)
             self.set_lights(new_colour)
 
     def blank(self):
-        self.set_lights('off')
+        self.set_lights('alloff')
 
     def draw_attention_to_state_change(self):
         for i in range(3):
             self.set_lights('changestate')
-            self.set_lights('off')
+            self.set_lights('alloff')
 
-    def set_lights_RPi(self, colour_or_colours_name):
-        lamp_settings = dict(zip(['red', 'yellow', 'green'], traffic_light_lamp_configs[colour_or_colours_name]))
-        os.system("./clewarecontrol -d 901880 -c 1 -as 0 {red} -as 1 {yellow} -as 2 {green}".format(**lamp_settings))
+    # def set_lights_RPi(self, colour_or_colours_name):
+    #     lamp_settings = dict(zip(['red', 'yellow', 'green'], traffic_light_lamp_configs[colour_or_colours_name]))
+    #     os.system("./clewarecontrol -d 901880 -c 1 -as 0 {red} -as 1 {yellow} -as 2 {green}".format(**lamp_settings))
 
     def set_lights(self, pattern_name):
-        pattern = traffic_light_lamp_configs[pattern_name]
+        pattern = conf['trafficlight']['lamppatterns'][pattern_name]
         lamps = 'R' if pattern[0] else ''
         lamps += 'Y' if pattern[1] else ''
         lamps += 'G' if pattern[2] else ''
@@ -88,6 +73,6 @@ class TrafficLight:
             # can be yellow if there is a comms failure and no failed tests up til now
             if any(passed is None for passed in env_results):
                 comms_failure = True
-        colours = 'green' if all_passed else "yellow" if comms_failure else "red"
-        colours = 'commserror' if comms_failure and not all_passed else colours
+        colours = 'green' if all_passed else "commserror" if comms_failure else "red"
+        colours = 'commserrorandfailures' if comms_failure and not all_passed else colours
         self.change_lights(colours)
