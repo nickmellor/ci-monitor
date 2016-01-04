fc6e36243a879ae80934f2199653e4a206f4497f
Nick Mellor <nicholas.mellor@medibank.com.au>
2016-01-04 16:38:25 +1100

    tidying

--- sound.py
+++ sound.py
@@ -4,36 +4,21 @@ import os
 import pyaudio
 import wave
 
-# define stream chunk
-chunk = 1024  
+chunk = 1024
 
 
 def playwav(wav_filename):
-    # open a wav format music
-    f = wave.open(os.path.join('sounds', wav_filename),"rb")
-
-    # instantiate PyAudio
-    p = pyaudio.PyAudio()  
-
-    # open stream
-    stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
-                    channels=f.getnchannels(),
-                    rate=f.getframerate(),
-                    output=True)
-    # read data
-    data = f.readframes(chunk)  
-
-    # play stream
-    while data != '':  
+    wav_file = wave.open(os.path.join('sounds', wav_filename),"rb")
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=audio.get_format_from_width(wav_file.getsampwidth()),
+                        channels=wav_file.getnchannels(), rate=wav_file.getframerate(), output=True)
+    data = wav_file.readframes(chunk)
+    while data != '':
         stream.write(data)  
-        data = f.readframes(chunk)  
-
-    # stop stream
-    stream.stop_stream()  
+        data = wav_file.readframes(chunk)
+    stream.stop_stream()
     stream.close()  
-
-    # close PyAudio
-    p.terminate()
+    audio.terminate()
 
 if __name__ == '__main__':
     playwav('applause.wav')
