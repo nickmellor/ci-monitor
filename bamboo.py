4933c15b40400cdacb37d29a84c17448a86c5466
Nick Mellor <nicholas.mellor@medibank.com.au>
2016-02-04 08:15:06 +1100

    refactor bamboo state logic

--- bamboo.py
+++ bamboo.py
@@ -25,7 +25,7 @@ class Bamboo:
     def previous_failure_count_cache_key(uri):
         return 'BambooConnection:{uri}'.format(uri=uri)
 
-    def task_result(self, environment, job, uri):
+    def bamboo_job_result(self, environment, job, uri):
         successfully_connected = self.get_connection_state(uri)
         try:
             if proxies:
@@ -40,6 +40,7 @@ class Bamboo:
                 message = message.format(signaller="OMS",  env=environment, job=job, uri=uri, exception=e)
                 logger.warning(message)
                 self.store_connection_state(uri, False)
+            return None
         else:
             self.store_connection_state(uri, True)
             logger.info("response {response} from {job} ({uri})"
@@ -48,21 +49,36 @@ class Bamboo:
             logger.info("Tests passing({job}): {results}"
                         .format(job=job, results=results['successfulTestCount']))
             failures = results['failedTestCount']
-            if failures:
-                logger.warning("*** Tests failing ({job}): {failures} ***"
-                    .format(job=job, failures=failures))
+            if failures != self.previous_failure_count(uri):
+                if failures != 0:
+                    logger.warning(
+                        "{job}: *** Tests failing: {failures} ***\n"
+                        "No further warnings will be given until number of failures changes"
+                            .format(job=job, failures=failures))
+                else:
+                    logger.warning("*** NEW!! Tests all passing! ({job}) ***\n".format(job=job))
+                self.store_failure_count(uri, failures)
             else:
                 logger.info("All active tests passed ({job})".format(job=job))
-            logger.info("Skipped tests ({job}): {skipped}"
+            logger.info("{job}: skipped tests: {skipped}"
                         .format(job=job, skipped=results['skippedTestCount']))
             return results['successful']
-        self.store_connection_state(uri, successfully_connected)
 
     def get_connection_state(self, uri):
         return State.retrieve(self.previous_connection_cache_key(uri), True)
 
     def store_connection_state(self, uri, was_connected):
         State.store(self.previous_connection_cache_key(uri), was_connected)
+
+    def previous_failure_count(self, uri):
+        return State.retrieve(self.previous_failures_cache_key(uri), 0)
+
+    def store_failure_count(self, uri, n):
+        State.store(self.previous_failures_cache_key(uri), n)
+
+    @staticmethod
+    def previous_failures_cache_key(uri):
+        return 'BambooFailures:{uri}'.format(uri=uri)
 
     def all_results(self):
         results = {}
@@ -75,9 +91,9 @@ class Bamboo:
         uri_template = bamboo_detail.get('uri')
         for job, tag in bamboo_detail['jobs'].items():
             uri = uri_template.format(tag=tag)
-            result = self.task_result(environment, job, uri)
+            result = self.bamboo_job_result(environment, job, uri)
             results.update({job: result})
             if self.get_connection_state(uri):
                 logger.info("{environment}: '{job}', Bamboo tag {tag}, result is '{result}'"
-                            .format(environment=environment, job=job, tag=tag, result=result))
+                            .format(environment=environment, job=job, tag=tag, result='passed' if result else 'failed tests'))
         return results
--- traffic.py
+++ traffic.py
@@ -18,20 +18,26 @@ class TrafficLight:
         self.device_id = tlight_settings['id']
         logger.info("Traffic light device '{device}' is being used by signal '{signaller}'"
                     .format(device=self.device_id, signaller=signaller))
-        # last time we knew, traffic light was connected
-        self.was_connected = State.retrieve(self.connected_status_storage_key(), True)
-        self.state = State.retrieve(self.previous_state_key())
 
-    def connected_status_storage_key(self):
-        return 'tlight:{device_id}:connected'.format(device_id=self.device_id)
+    def previous_state(self):
+        return State.retrieve(self.previous_state_cache_key())
 
-    def previous_state_key(self):
+    def store_state(self, state):
+        State.store(self.previous_state_cache_key(), state)
+
+    def previous_state_cache_key(self):
         return 'tlight:{device_id}:previousState'.format(device_id=self.device_id)
+
+    def previously_connected(self):
+        return State.retrieve(self.connected_cache_key(), True)
+
+    def connected_cache_key(self):
+        return 'tlight:{device_id}:connected'.format(device_id=self.device_id)
 
     def blink(self):
         self.all_lamps_off()
         sleep(settings['blinktime_secs'])
-        self._set_lamps(self.state, monitor=False)
+        self._set_lamps(self.previous_state(), monitor=False)
 
     def all_lamps_off(self):
         self._set_lamps('blank', monitor=False)
@@ -40,36 +46,41 @@ class TrafficLight:
         for i in range(3):
             self._set_lamps('changestate', monitor=False)
             self._set_lamps('blank', monitor=False)
-        if self.was_connected:
+        if self.previously_connected():
             logger.info("{signaller}: device '{device}': light changing from '{old}' to '{new}'"
                         .format(signaller=self.signaller, device=self.device_id,
-                                old=self.state, new=new_state))
+                                old=self.previous_state(), new=new_state))
         self._set_lamps(new_state)
-        State.store(self.previous_state_key(), new_state)
+        self.set_state(new_state)
+
+    def set_state(self, new_state):
+        State.store(self.previous_state_cache_key(), new_state)
 
     def set_lights(self, new_state):
-        if new_state != self.state:
+        if new_state != self.previous_state():
             self.lights_changing(new_state)
         else:
             self.blink()
 
     def _set_lamps(self, state, monitor=True):
         shell_command = self._shell_command(state)
-        if monitor and self.was_connected:
+        if monitor and self.previously_connected():
             logger.info("Executing shell command for traffic light: '{0}'".format(shell_command))
         stdout, error = subprocess.Popen(shell_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                          shell=True).communicate()
-        device_detected = not (stdout or error)
+        device_detected = not (stdout or error)  # device drivers missing/device missing not differentiated
         if not device_detected:
-            if self.was_connected:
+            if self.previously_connected():
                 message = "Signaller '{signaller}' traffic light '{device}' is not responding.\n" \
                           "No further warnings for this traffic light will be given until it is connected.\n"
                 message += "Message: '{message}'\n"
                 message = message.format(signaller=self.signaller, device=self.device_id,
                                          message=stdout.decode('UTF-8').strip())
                 logger.warning(message)
-                self.was_connected = False
-        State.store(self.connected_status_storage_key(), device_detected)
+        self.store_connection_status(device_detected)
+
+    def store_connection_status(self, device_detected):
+        State.store(self.connected_cache_key(), device_detected)
 
     def _shell_command(self, state):
         lamp_pattern = settings['lamppatterns'][state]
