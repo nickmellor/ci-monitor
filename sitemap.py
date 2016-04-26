9959f71186ae9935d0c464b39139987e296abcae
Nick Mellor <nicholas.mellor@medibank.com.au>
2016-04-26 15:32:08 +1000

    checks for error text

--- sitemap.py
+++ sitemap.py
@@ -3,6 +3,8 @@ import requests
 import xml.etree.ElementTree as ET
 from logger import logger
 import yaml
+from html.parser import HTMLParser
+import re
 
 
 class Sitemap:
@@ -14,14 +16,14 @@ class Sitemap:
         faults = []
         for sitemap_name, sitemap in self.sitemaps.items():
             for url in self.extracted_urls(sitemap):
-                url_response = requests.get(url, verify=False, proxies=proxies)
-                if not 200 <= url_response.status_code < 300:
-                    faults += (sitemap_name, url, url_response.status_code)
+                response = requests.get(url, verify=False, proxies=proxies)
+                if errorpage(response):
+                    faults += (sitemap_name, url, response.status_code)
         if faults:
             message = ['*' * 40]
             message.extend(faults)
             message.append('*' * 40)
-            logger.error(faults)
+            logger.error('\n'.join(message))
 
     def extracted_urls(self, uri):
         sitemap_as_string = self.sitemap_xml(uri)
@@ -40,11 +42,39 @@ class Sitemap:
             return None
 
 
+def errorpage(response):
+    """error pages return 200 so need to check error page text"""
+    res = not 200 <= response.status_code < 300
+    text = tidy(strip_tags(response.text))
+    res = res and not('page not found' in text)
+    res = res and not('could not process request' in text)
+    return res
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
+def tidy(s):
+    return re.sub('\s+', ' ', s.lower().strip())
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ' '.join(self.fed)
+
 if __name__ == '__main__':
     with open('conf.yaml') as config_file:
         settings = yaml.load(config_file)['signallers']['SITEMAP_TEST']['sitemap']
     s = Sitemap(settings)
     s.poll()
-    # repo = Repo('scratch/repos/integration-services')
-    # d = repo.heads['develop'].commit.committed_date
-    # print(time.gmtime(d))
