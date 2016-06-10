31dec7234a1ee9aa3ab7b0f52beb135396118f50
Nick Mellor <nicholas.mellor@medibank.com.au>
2016-06-10 15:14:51 +1000

    Reset framework every midnight (to repeat suppressed errors once daily)

--- ci_monitor.py
+++ ci_monitor.py
@@ -1,5 +1,6 @@
 from time import sleep
 import sys
+import datetime
 
 import schedule
 
@@ -7,35 +8,47 @@ from conf import o_conf, config_changed
 from indicator import Indicator
 from utils.logger import logger, configure_logging
 
-logger.warning('CI Monitor restarted')
+def day_of_month():
+    return datetime.datetime.now().day
+
+last_day = day_of_month()
+
 while True:
+    logger.warning('CI Monitor restarted')
     configure_logging()
     schedule.clear()
     indicators = []
     for name, settings in o_conf().indicators.items():
         indicators.append(Indicator(name, settings))
-    while not config_changed():
+    while True:
+        if config_changed():
+            logger.warning('Config changed!')
+            break
+        today = day_of_month()
+        if today != last_day:
+            logger.warning('Date flip-over!')
+            last_day = today
+            break
         for indicator in indicators:
-            # try:
+            try:
                 indicator.run()
-            # except KeyboardInterrupt as e:
-            #     logger.warning('Interrupted by Ctrl+C: exiting...')
-            #     sys.exit()
-            # except Exception as e:
-            #     logger.error("Unhandled exception(s) in CI-Monitor:\n{0}".format(repr(e)))
-            #     sleep(o_conf().defaults.errorheartbeat_secs)
-            # else:
+            except KeyboardInterrupt as e:
+                logger.warning('Interrupted by Ctrl+C: exiting...')
+                sys.exit()
+            except Exception as e:
+                logger.error("Unhandled exception(s) in CI-Monitor:\n{0}".format(repr(e)))
+                sleep(o_conf().defaults.errorheartbeat_secs)
+            else:
                 sleep(o_conf().defaults.heartbeat_secs)
-    logger.warning('Config changed!')
 
-# TODO: unit tests
+
+
+# TODO: unit tests(!)
 # TODO: repeat suppressed "indications" daily (suppressed repeat errors)
-# TODO: consider making merge atomic. Leads to repetitive config but reduces code exceptions and long delays
+# TODO: test recovery from persistent error (e.g. build fixed)
 # TODO: exclusions list for merges(?)
-# TODO: deal with unhandled exceptions in new architecture
-# TODO: check traffic light transitions in new architecture
-# TODO: traffic light blink (indicator) in new architecture
-# TODO: unhandled exceptions in indicators
+# TODO: check traffic light transitions
+# TODO: traffic light blink
 # TODO: factor out message building (esp. indicator name)
 # TODO: ScheduleSetter log to info when polling monitor (move from indicator)
 # TODO: monitors return results as Python objects; indicator can output
